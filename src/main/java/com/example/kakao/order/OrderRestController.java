c95cd1e869e8b70bf4faa50e68e57218c84224d7
tofu927 <sfg0729@gmail.com>
2024-01-25 11:40:49 +0900

    Update OrderRestController.java

--- src/main/java/com/example/kakao/order/OrderRestController.java
+++ src/main/java/com/example/kakao/order/OrderRestController.java
@@ -1,7 +1,10 @@
 package com.example.kakao.order;
 
+import com.example.kakao._core.utils.ApiUtils;
+import com.example.kakao.user.User;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RestController
@@ -17,8 +20,11 @@ public class OrderRestController {
 
 
     // TODO : 주문 목록 모두 삭제
-    //@PostMapping("/orders/clear")
-
-
+    @PostMapping("/orders/clear")
+    public ResponseEntity<?> clear(User user) {
+        orderService.clear(user);
+        ApiUtils.ApiResult<?> apiResult = ApiUtils.success(null);
+        return ResponseEntity.ok(apiResult);
+    }
 
 }
