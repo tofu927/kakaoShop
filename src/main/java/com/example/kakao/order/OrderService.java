fe35bfbf812087b6c9eb5e4a6cecd11cf73dc1ca
tofu927 <sfg0729@gmail.com>
2024-01-25 11:47:21 +0900

    Update OrderService.java

--- src/main/java/com/example/kakao/order/OrderService.java
+++ src/main/java/com/example/kakao/order/OrderService.java
@@ -1,8 +1,10 @@
 package com.example.kakao.order;
 
+import com.example.kakao._core.errors.exception.Exception500;
 import com.example.kakao.cart.CartJPARepository;
 import com.example.kakao.option.OptionJPARepository;
 import com.example.kakao.order.item.ItemJPARepository;
+import com.example.kakao.user.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -17,4 +19,13 @@ public class OrderService {
     private final OptionJPARepository optionJPARepository;
     private final CartJPARepository cartJPARepository;
 
+    @Transactional
+    public void clear(User user) {
+        try {
+            orderJPARepository.deleteByUserId(user.getId());
+        }catch (Exception e){
+            throw new Exception500("주문 목록 초기화 실패 : "+e.getMessage());
+        }
+
+    }
 }
